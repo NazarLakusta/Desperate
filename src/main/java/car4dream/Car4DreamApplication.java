45163e8f69b1df82a53ee2740d4039e1ddba0073
NazarLakusta <lakusta.nazar.clg@chnu.edu.ua>
2023-06-25 09:58:27 +0300

    Create start-up file

---
+++ src/main/java/car4dream/Car4DreamApplication.java
@@ -0,0 +1,11 @@
+package car4dream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Car4DreamApplication{
+    public static void main(String[] args) {
+        SpringApplication.run(Car4DreamApplication.class,args);
+    }
+}
